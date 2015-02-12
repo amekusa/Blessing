51492c60cb81f35df2e0903251b53d2630bad6dd
amekusa <post@amekusa.com>
2015-02-12 13:56:39 +0900

    Update the package

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var project = require('package.json').name.toLowerCase();
+var project = require('./package.json').name.toLowerCase();
 
 var gulp = require("gulp");
 var plugins = require("gulp-load-plugins")();
@@ -7,33 +7,31 @@ gulp.task("default", ["build", "watch"]);
 
 gulp.task("build", ["compile", "compress"]);
 
-gulp.task("compile", ["bundle"], function() {
-	return gulp.src("src/" + project + ".less")
+//gulp.task("compile", ["bundle"], function() {
+gulp.task("compile", function() {
+	return gulp.src("./src/main.less")
 			.pipe(plugins.less({
 				
 			}))
-			.pipe(plugins.rename({extname: ".css"}))
-			.pipe(gulp.dest("dist"))
-			.pipe(plugins.notify("Less Compile succeeded!"));
+			.pipe(plugins.autoprefixer("last 2 versions"))
+			.pipe(plugins.rename(project + ".css"))
+			.pipe(gulp.dest("dist"));
 });
 
 gulp.task("bundle", function() {
-	return gulp.src("src/main.less")
+	return gulp.src("./src/main.less")
 			.pipe(plugins.bundle-assets())
-			.pipe(gulp.dest("dist"))
+			.pipe(gulp.dest("dist"));
 });
 
 gulp.task("compress", ["compile"], function() {
-	return gulp.src("src/style.css")
-			.pipe(plugins.cssimport())
-			.pipe(plugins.autoprefixer("last 2 versions"))
+	return gulp.src("./dist/" + project + ".css")
 			.pipe(plugins.minifyCss())
 			.pipe(plugins.rename({extname: ".min.css"}))
 			.pipe(gulp.dest("dist"));
 });
 
 gulp.task("watch", function() {
-	browserSync.init
-		
-	gulp.watch(["src/*.less"], ["lessc"]);
+	//browserSync.init
+	gulp.watch(["src/*.less"], ["compile"]);
 });
