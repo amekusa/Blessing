93cab7dddbbdbe1c835ab83116b11ddac0607661
amekusa <post@amekusa.com>
2015-06-15 19:41:25 +0900

    Cleanup gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,59 @@
-var project = require('./package.json').name.toLowerCase();
+var paths = {
+	package : './package.json',
+	kssConf : './kss-config.json',
+	src : './src'
+};
 
-var gulp = require("gulp");
-var plugins = require("gulp-load-plugins")();
-var purge = require("gulp-css-purge"); // gulp-load-plugins cannot load this :(
-var styleguide = require('sc5-styleguide');
+var package = require(paths.package);
+var project = package.name.toLowerCase();
+var kssConf = require(paths.kssConf);
 
-gulp.task("default", ["build", "watch"]);
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var purge = require('gulp-css-purge'); // gulp-load-plugins cannot load this :(
 
-gulp.task("build", ["compile", "compress"]);
+gulp.task('default', ['build', 'watch']);
+gulp.task('build', ['compile', 'compress']);
 
-//gulp.task("compile", ["bundle"], function() {
-gulp.task("compile", function() {
-	return gulp.src("./src/main.less")
-			.pipe(plugins.sourcemaps.init())
-			.pipe(plugins.less({
-				sourceMap: true
-			}))
-			.pipe(purge())
-			.pipe(plugins.autoprefixer("last 2 versions"))
-			.pipe(plugins.sourcemaps.write())
-			.pipe(plugins.rename(project + ".css"))
-			.pipe(gulp.dest("dist"));
+gulp.task('compile', function() {
+	return gulp.src(paths.src + '/main.less')
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.less({
+			sourceMap: true
+		}))
+		.pipe(purge())
+		.pipe(plugins.autoprefixer('last 2 versions'))
+		.pipe(plugins.sourcemaps.write())
+		.pipe(plugins.rename(project + '.css'))
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task("bundle", function() {
-	return gulp.src("./src/main.less")
-			.pipe(plugins.bundle-assets())
-			.pipe(gulp.dest("dist"));
+gulp.task('bundle', function() {
+	return gulp.src(paths.src + '/main.less')
+		.pipe(plugins.bundle-assets())
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task("compress", ["compile"], function() {
-	return gulp.src("./dist/" + project + ".css")
-			.pipe(plugins.minifyCss())
-			.pipe(plugins.rename({extname: ".min.css"}))
-			.pipe(gulp.dest("dist"));
+gulp.task('compress', ['compile'], function() {
+	return gulp.src('./dist/' + project + '.css')
+		.pipe(plugins.minifyCss())
+		.pipe(plugins.rename({extname: '.min.css'}))
+		.pipe(gulp.dest('dist'));
 });
 
-gulp.task("docs", ["compile"], function() {
-	plugins.run('kss-node --config kss-config.json').exec();
+gulp.task('docs', ['compile'], function() {
+	plugins.run('kss-node --config ' + paths.kssConf).exec();
 });
 
-gulp.task("watch", function() {
-	//browserSync.init
-	gulp.watch(["src/*.less"], ["compile"]);
+gulp.task('watch', function() {
+	gulp.watch([
+		paths.src + '/**/*.less',
+		paths.package
+	], ['compile']);
+
+	gulp.watch([
+		kssConf.source + '/' + kssConf.mask,
+		kssConf.source + '/' + kssConf.homepage,
+		paths.kssConf
+	], ['docs']);
 });
